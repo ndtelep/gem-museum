b13b400db79d23d12d5f81b02aef4e054faecb63
ndtelep <ndtelep@gmail.com>
2022-12-02 14:50:17 -0700

    added more card styling

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -14,22 +14,22 @@ function ItemCard({ item }) {
   const { game_used } = item;
 
   return (
-    <div class="card hover-translate-y column is-quarter mt-5">
-      <header class="card-header is-capitalized has-text-centered">
-        {/* <Link to={`/item_details/${id}`}> */}
-        <p class="is-size-4 has-text-weight-bold">
-          {team} {category}
-        </p>
-        {/* </Link> */}
-      </header>
-      <Link to={`/item_details/${id}`}>
-        <div class="card-image">
-          <figure class="image is-5by4">
-            <img class="px-5" src={image_url} alt="item" />
-          </figure>
-        </div>
-      </Link>
+    <div class="card hover-translate-y column is-one-quarter mt-5">
       <div class="card-content">
+        <header class="card-header is-capitalized has-text-centered">
+          {/* <Link to={`/item_details/${id}`}> */}
+          <p class="is-size-4 has-text-weight-bold">
+            {team} {category}
+          </p>
+          {/* </Link> */}
+        </header>
+        <Link to={`/item_details/${id}`}>
+          <div class="card-image">
+            <figure class="image is-5by4">
+              <img class="px-5" src={image_url} alt="item" />
+            </figure>
+          </div>
+        </Link>
         <div class="media">
           <div class="media-left"></div>
         </div>
@@ -37,12 +37,8 @@ function ItemCard({ item }) {
           {brand}, {era}
         </div>
         <footer class="card-footer">
-          {game_used && (
-              <p class="card-footer-item">Game Used</p>
-          )}
-          {for_sale && (
-              <p class="card-footer-item">For Sale!</p>
-          )}
+          {game_used && <p class="card-footer-item">Game Used</p>}
+          {for_sale && <p class="card-footer-item">For Sale!</p>}
         </footer>
       </div>
     </div>
