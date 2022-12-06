fe75a60e8de1ae25307a7f982de1364fb666350e
ndtelep <ndtelep@gmail.com>
2022-12-06 14:03:42 -0700

    added dummy user page

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function UserPage() {
   return (
-    <div>UserPage</div>
-  )
+    <div>
+      {" "}
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7"></div>
+          <div class="col-lg-5">
+            <h1 class="is-size-2 has-text-weight-bold has-text-primary">
+              User's collection
+            </h1>
+            <h3 class="is-size-3 is-italic has-text-centered">
+              Your collected items
+            </h3>
+            <div>
+              <p>a users items will be displayed here</p>
+            </div>
+            <h3 class="is-size-3 is-italic has-text-centered">
+              Your watched items
+            </h3>
+            <div>
+              <p>a users watches will be displayed here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default UserPage
+export default UserPage;
